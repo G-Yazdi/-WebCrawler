e42c1941b1d946f88c629d6134245bc24b11851b
G-Yazdi <hg.yazdi@gmail.com>
2024-01-24 20:44:12 +0330

    adds junit 5 to the project

---
+++ crawler/src/test/java/com/yazdi/crawler/CrawlerTest.java
@@ -0,0 +1,20 @@
+package com.yazdi.crawler;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CrawlerTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
