9041d314e19670d63d81e54deef57594bc31c1c1
G-Yazdi <hg.yazdi@gmail.com>
2024-01-24 21:43:57 +0330

    Adds JsoupCrawler class

---
+++ crawler/src/main/java/com/yazdi/crawler/JsoupCrawler.java
@@ -0,0 +1,11 @@
+package com.yazdi.crawler;
+
+public class JsoupCrawler implements Crawler {
+
+	@Override
+	public void crawl(String domain, String url, int maxDepth) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
