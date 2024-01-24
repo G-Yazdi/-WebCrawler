0441a2501eaef8d8fa9de3792f878765cd27d3b3
G-Yazdi <hg.yazdi@gmail.com>
2024-01-24 21:41:47 +0330

    Adds test for checking invalid values for max depth

--- crawler/src/test/java/com/yazdi/crawler/CrawlerTest.java
+++ crawler/src/test/java/com/yazdi/crawler/CrawlerTest.java
@@ -1,7 +1,8 @@
 package com.yazdi.crawler;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -9,12 +10,15 @@ import org.junit.jupiter.api.Test;
  */
 public class CrawlerTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+	private static Crawler crawler;
+	
+	@BeforeAll 
+	public static void init() {
+	}
     @Test
-    public void shouldAnswerWithTrue()
+    public void invalidMaxDepthExceptionTest()
     {
-        assertTrue( true );
+    	Throwable exception = assertThrows(RuntimeException.class, () -> crawler.crawl("", "", -1));
+		assertEquals("Invalid max depth value: The value of max depth should be an integer and greater than 0", exception.getMessage());
     }
 }
